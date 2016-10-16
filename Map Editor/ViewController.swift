c61530f66964643d980d343a93185549fe7a133d
Khoa Nguyen <vankhoa0603@gmail.com>
2016-10-16 11:13:20 +0700

    Initial Commit

---
+++ Map Editor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Map Editor
+//
+//  Created by Khoa on 10/16/16.
+//  Copyright © 2016 Khoa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
